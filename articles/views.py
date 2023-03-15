47123ffced0d7c47445430d079b67d344e064ab9
A-MIKs <amiks262@gmail.com>
2023-03-15 12:53:27 +0100

    initial commit

---
+++ articles/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView, FormView, DetailView)
+from .models import Article
+from django.urls import reverse_lazy, reverse
+from django.views.generic.edit import (
+    CreateView, UpdateView, DeleteView)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,)
+from .forms import CommentForm
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "articles/article_list.html"
+
+class ArticleDetailView(LoginRequiredMixin, DetailView, View):
+    model = Article
+    template_name = "articles/article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+class ArticleUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    fields = (
+        "title",
+        "body",
+    )
+    template_name = "articles/article_edit.html"
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(
+    LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = "articles/article_delete.html"
+    success_url = reverse_lazy("articles/article_list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = "articles/article_new.html"
+    fields = (
+        "title",
+        "body",
+    )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class CommentGet(DetailView):
+    model = Article
+    template_name = "articles/article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+    
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name= "articles/article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, *kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("articles/article_detail", kwargs={"pk": article.pk})
+    
+
